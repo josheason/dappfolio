4220d9b7a86b7785375b3eaea8c712727d998318
Josh Eason <josheason@Joshs-MacBook-Pro.local>
2021-01-10 13:45:55 -0500

    Updates

---
+++ precache-manifest.34092ccee938dd6d9b44f791bc36a71c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af9d4ed41ec307c4704e0c3015bf6e73",
+    "url": "/dappfolio/index.html"
+  },
+  {
+    "revision": "c0b923125e4c1b75fdfd",
+    "url": "/dappfolio/static/css/main.4300b6f2.chunk.css"
+  },
+  {
+    "revision": "02599e6b65654b8dc228",
+    "url": "/dappfolio/static/js/2.1f4e5c99.chunk.js"
+  },
+  {
+    "revision": "c0b923125e4c1b75fdfd",
+    "url": "/dappfolio/static/js/main.50f73a5f.chunk.js"
+  },
+  {
+    "revision": "7fcb818ec6903f71ee7d",
+    "url": "/dappfolio/static/js/runtime-main.9404c3f6.js"
+  },
+  {
+    "revision": "d75a301aace970c3cba414d0609079e9",
+    "url": "/dappfolio/static/media/connected1.d75a301a.png"
+  },
+  {
+    "revision": "66a0ceb4acc0ff8b2c7fa9405f231dcd",
+    "url": "/dappfolio/static/media/dai1.66a0ceb4.png"
+  },
+  {
+    "revision": "6faae170ac10f92b759c61985312d88c",
+    "url": "/dappfolio/static/media/daic1.6faae170.png"
+  },
+  {
+    "revision": "091c1c215c87a2bee495da56a643ba2c",
+    "url": "/dappfolio/static/media/eth123.091c1c21.png"
+  },
+  {
+    "revision": "b0894bad59b76f77a86e22ab523e8f90",
+    "url": "/dappfolio/static/media/metaconnect1.b0894bad.png"
+  },
+  {
+    "revision": "3c06d4ad105d5f2e9289e0f01c6d1ccc",
+    "url": "/dappfolio/static/media/vaultLogo.3c06d4ad.png"
+  }
+]);
